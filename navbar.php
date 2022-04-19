c5507756a01fe09e634cbf7149993d75094b7833
hossam alhamidi <52385746+hossamAlhamidi@users.noreply.github.com>
2022-04-20 02:08:36 +0300

    nav dark

--- navbar.php
+++ navbar.php
@@ -20,7 +20,7 @@
 
 
 
-      <nav class="navbar navbar-expand-sm navbar-light">
+      <nav class="navbar navbar-expand-sm navbar-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="user.php" style = "font-family:Cursive;">La Galérie</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
